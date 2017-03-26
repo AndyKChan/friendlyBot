7ca86fac10d094a45cc96cbb3f8193c1b9816949
AndyKChan <andykinhangchan@gmail.com>
2017-03-25 20:47:58 -0700

    created base recognition for LUIS dialogues

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/603163cb-a45c-4308-a518-6d48d0b65618?subscription-key=ea2c31d50ef04c339bf5637ed3dcc758&timezoneOffset=0.0&verbose=true&q=';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+
+bot.dialog('/', dialog);
+
+dialog.matches('Book_Holiday', builder.DialogAction.send('Sure booking holiday!'));
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));
