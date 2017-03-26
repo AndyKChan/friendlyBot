1afd2c772ca3cab8d3cadec119d6bdf42c311f26
AndyKChan <andykinhangchan@gmail.com>
2017-03-26 00:55:55 -0700

    uploading picture works properly, returns image in URL form

--- app.js
+++ app.js
@@ -27,20 +27,27 @@ server.post('/api/messages', connector.listen());
 var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/603163cb-a45c-4308-a518-6d48d0b65618?subscription-key=ea2c31d50ef04c339bf5637ed3dcc758&timezoneOffset=0.0&verbose=true&q=';
 var recognizer = new builder.LuisRecognizer(model);
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
-var intents = new builder.IntentDialog();
 
-bot.dialog('/', intents);
 
-intents.matches(/^change name/i, [
-    function (session) {
-        session.beginDialog('/profile');
+bot.dialog('/', dialog);
+
+dialog.matches('Change_Name', [
+    function (session, results) {
+		if(results.entities && results.entities.length && results.entities[0]){
+			var userName =  results.entities[0].entity;
+			session.userData.name = userName;
+			session.send('Hello %s!', userName)
+		}
+		else {
+			session.beginDialog('/profile');
+		}
     },
     function (session, results) {
         session.send('Ok... Changed your name to %s', session.userData.name);
     }
 ]);
-intents.matches('Book_Holiday', builder.DialogAction.send('Sure, booking your holiday!'));
-intents.onDefault([
+
+dialog.onDefault([
     function (session, args, next) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
@@ -63,15 +70,17 @@ bot.dialog('/profile', [
     }
 ]);
 
-intents.matches(/^upload/i, [
+dialog.matches(/^upload/i, [
 	function(session){
 		builder.Prompts.attachment(session, "Upload a picture of food for me to analyze!");
 	},
 	function(session, results){
+		console.log(results.response[0].contentUrl);
 		session.userData.foodPic = results.response;
+		session.send(results.response[0].contentUrl);
 		session.endDialog();
 	}
 ]);
 
-
+dialog.matches('Book_Holiday', builder.DialogAction.send('Sure, booking your holiday!'));
 /*dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));*/
