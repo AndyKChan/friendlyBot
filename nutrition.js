9efee34b828a2b8ce4b42fa75d373cf89fcd8d1b
kgrover97 <grover.karan97@gmail.com>
2017-03-26 02:20:46 -0700

    Successfully getting info for nutrients in JSON format

--- nutrition.js
+++ nutrition.js
@@ -1,31 +1,94 @@
-
-var search = 'cookie';
-var http = require('http');
-
-var options = {
-  hostname: 'api.nutritionix.com',
-  port: 80,
-  path: '/search/' + encodeURI(search) + '?results=0:1&fields=item_name,brand_name,item_id,nf_calories&appId=f0efc5ac&appKey=75f8e573f3d4e21f9b74334e590dc47a',
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  }
-};
-
-var req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
+// This will perform a search. The object passed into this function
+// can contain all the perameters the API accepts in the `POST /v2/search` endpoint
+var NutritionixClient = require('nutritionix');
+var nutritionix = new NutritionixClient({
+    appId: 'f0efc5ac',
+    appKey: '75f8e573f3d4e21f9b74334e590dc47a'
+    // debug: true, // defaults to false
 });
 
-req.on('error', (e) => {
-  console.log(`problem with request: ${e.message}`);
+// This will perform a search. The object passed into this function
+// can contain all the perameters the API accepts in the `POST /v2/search` endpoint
+nutritionix.search({
+  q:'salad',
+  // use these for paging
+  limit: 10,
+  offset: 0,
+  fields: [
+   "item_name",
+   "brand_name",
+   "nf_calories",
+   "nf_sodium",
+   "item_type"
+ ],
+
+  // controls the basic nutrient returned in search
+  search_nutrient: 'calories'
+}).then(results => {
+  console.log(results);
+
+
 });
 
-req.end();
+function successHandler(data){
+  console.log("hello");
+}
+
+function errorHandler(data){
+  console.log("hello");
+}
+
+//
+//
+// var querystring = require('querystring');
+// var https = require('https');
+//
+// var host = "api.nutritionix.com";
+// var appKey = "75f8e573f3d4e21f9b74334e590dc47a";
+// var appId = "f0efc5ac";
+//
+// function performRequest(endpoint, method, data, success) { //method is GET or POST, data is food item
+//   var dataString = JSON.stringify(data);
+//   var headers = {
+//     'Content-Type': 'application/json',
+//   };
+//
+//   // if (method == 'GET') {
+//     endpoint += '?' + querystring.stringify(data);
+//   // }
+//
+//   var options = {
+//     host: host,
+//     path: endpoint,
+//     method: method,
+//     headers: headers
+//   };
+//
+//   var req = https.request(options, function(res) {
+//     res.setEncoding('utf-8');
+//
+//     var responseString = '';
+//
+//     res.on('data', function(data) {
+//       responseString += data;
+//     });
+//
+//     res.on('end', function() {
+//       console.log(responseString);
+//       var responseObject = JSON.parse(responseString);
+//       success(responseObject);
+//     });
+//   });
+//
+//   req.write(dataString);
+//   req.end();
+// }
+//
+// performRequest('/v1_1/search/'+'cookie', 'GET', {
+//   "fields" : "item_name",
+//   "appId":appId,
+//   "appKey":appKey,
+//   // "query":"Cookies `n Cream"
+// }, function(data) {
+//   console.log('Fetched and called bot successfully!!!');
+// });
